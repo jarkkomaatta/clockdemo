670d1ff10dd848f7fe72d079516af6f4388bd4ae
jarkkomaatta <127930482+jarkkomaatta@users.noreply.github.com>
2023-09-19 11:07:54 +0300

    clockdemo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  
+  const [Time, setTime] = useState(new Date())
+  
+  useEffect(() => {
+    const id = setInterval(() => {
+      setTime(new Date())
+    }, 1000)
+    return () => {
+      clearInterval(id)
+    }
+  }, [])
+  
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{margin: '30px',fontSize: '60px'}}>
+      {Time.toLocaleTimeString()}
     </div>
   );
 }
